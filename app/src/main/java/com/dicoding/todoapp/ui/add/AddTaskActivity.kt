eb45391094364d02c5e814f784b2ed1185216862
KrishnanditoLksn <krislaksana123@gmail.com>
2025-02-08 22:07:28 +0700

    feat: insert task into database

--- app/src/main/java/com/dicoding/todoapp/ui/add/AddTaskActivity.kt
+++ app/src/main/java/com/dicoding/todoapp/ui/add/AddTaskActivity.kt
@@ -1,25 +1,33 @@
 package com.dicoding.todoapp.ui.add
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.dicoding.todoapp.R
+import com.dicoding.todoapp.data.Task
+import com.dicoding.todoapp.ui.ViewModelFactory
+import com.dicoding.todoapp.ui.list.TaskActivity
 import com.dicoding.todoapp.utils.DatePickerFragment
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Calendar
+import java.util.Locale
 
 class AddTaskActivity : AppCompatActivity(), DatePickerFragment.DialogDateListener {
     private var dueDateMillis: Long = System.currentTimeMillis()
+    private lateinit var addTaskViewModel: AddTaskViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
 
         supportActionBar?.title = getString(R.string.add_task)
-
+        val factory = ViewModelFactory.getInstance(this)
+        addTaskViewModel = ViewModelProvider(this, factory)[AddTaskViewModel::class.java]
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -31,8 +39,35 @@ class AddTaskActivity : AppCompatActivity(), DatePickerFragment.DialogDateListen
         return when (item.itemId) {
             R.id.action_save -> {
                 //TODO 12 : Create AddTaskViewModel and insert new task to database
-                true
+                val taskTitle = findViewById<TextView>(R.id.add_ed_title).text.toString()
+                val taskDescription = findViewById<TextView>(R.id.add_ed_description)
+
+
+                val dateTextView = findViewById<TextView>(R.id.add_tv_due_date)
+                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                val dueDate = dateFormat.parse(dateTextView.text.toString())
+                val dueDateMillis = dueDate?.time ?: System.currentTimeMillis()
+
+
+                val task = Task(
+                    title = taskTitle,
+                    description = taskDescription.text.toString(),
+                    dueDateMillis = dueDateMillis,
+                    isCompleted = false
+                )
+
+                addTaskViewModel.addTask(task)
+
+                val intent = Intent(
+                    this,
+                    TaskActivity::class.java
+                )
+
+                startActivity(intent)
+                false
             }
+
+
             else -> super.onOptionsItemSelected(item)
         }
     }
