7e6ad28b2dfe4c82ab14297cf098bb2e4866a777
KrishnanditoLksn <krislaksana123@gmail.com>
2025-02-10 15:37:57 +0700

    feat: send notification earliest task

--- app/src/main/java/com/dicoding/todoapp/notification/NotificationWorker.kt
+++ app/src/main/java/com/dicoding/todoapp/notification/NotificationWorker.kt
@@ -1,16 +1,26 @@
 package com.dicoding.todoapp.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
 import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.core.app.TaskStackBuilder
+import androidx.preference.PreferenceManager
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.dicoding.todoapp.R
 import com.dicoding.todoapp.data.Task
+import com.dicoding.todoapp.data.TaskRepository
 import com.dicoding.todoapp.ui.detail.DetailTaskActivity
 import com.dicoding.todoapp.utils.NOTIFICATION_CHANNEL_ID
 import com.dicoding.todoapp.utils.TASK_ID
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 
@@ -23,7 +33,10 @@ class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, p
         return TaskStackBuilder.create(applicationContext).run {
             addNextIntentWithParentStack(intent)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                getPendingIntent(task.id, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+                getPendingIntent(
+                    task.id,
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                )
             } else {
                 getPendingIntent(task.id, PendingIntent.FLAG_UPDATE_CURRENT)
             }
@@ -32,7 +45,47 @@ class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, p
 
     override fun doWork(): Result {
         //TODO 14 : If notification preference on, get nearest active task from repository and show notification with pending intent
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val isNotification = sharedPreferences.getBoolean("pref_key_notify", true)
+
+        if (isNotification) {
+            val repository = TaskRepository.getInstance(applicationContext)
+            val nearest = repository.getNearestActiveTask()
+            showNotification(nearest, applicationContext)
+        }
+
         return Result.success()
     }
 
+    private fun showNotification(task: Task, context: Context) {
+        val notificationManager =
+            context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val formattedDate = SimpleDateFormat("MMM dd, yyyy", Locale.getDefault())
+            .format(Date(task.dueDateMillis))
+
+
+        val notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notifications)
+            .setContentTitle(task.title)
+            .setContentText(context.getString(R.string.notify_content, formattedDate))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setAutoCancel(true)
+            .setContentIntent(getPendingIntent(task))
+            .build()
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+
+        notificationManager.notify(task.id, notification)
+    }
 }
